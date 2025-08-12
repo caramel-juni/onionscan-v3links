e9648a95f6f86ff311324314a228d2384c0287cb
juni <juniper@clarkedew.com>
2025-08-12 10:03:29 +0930

    removed old validation tests

--- utils/validation_test.go
+++ utils/validation_test.go
@@ -5,33 +5,6 @@ import (
 )
 
 func TestIsOnion(t *testing.T) {
-	// Valid v2 onion
-	test := IsOnion("maschqwtr4lqt2pj.onion")
-	if !test {
-		t.Errorf("%s should have matched!", "maschqwtr4lqt2pj.onion")
-	}
-
-	// Valid v2 onion with subdomains
-	test = IsOnion("subdomain.lots.of.maschqwtr4lqt2pj.onion")
-	if !test {
-		t.Errorf("%s should have matched!", "subdomain.lots.of.maschqwtr4lqt2pj.onion")
-	}
-
-	// Invalid onions (bad chars, wrong TLD)
-	test = IsOnion("thisis9notavalidonion.onion")
-	if test {
-		t.Errorf("%s should not have matched!", "thisis9notavalidonion.onion")
-	}
-
-	test = IsOnion("maschqwtr4lqt2pj.com")
-	if test {
-		t.Errorf("%s should not have matched!", "maschqwtr4lqt2pj.com")
-	}
-
-	test = IsOnion("www.thisisnotanonionitistoolong.onion")
-	if test {
-		t.Errorf("%s should not have matched!", "www.thisisnotanonionitistoolong.onion")
-	}
 
 	// Valid v3 onion
 	v3Onion := "dy3vbeecmljb2476ua26vasan24qezytsqb7rhiokqnw3jxzjsdse6ad.onion"
